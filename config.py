c3a1d9e5996ed1231c92d4ad87044f832f72b962
Toxic <gyaneshp606@gmail.com>
2026-07-01 10:49:32 +0530

    Iam-Toxicc

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ load_dotenv()
 class Config:
     API_ID = int(os.environ.get("API_ID", "39934421"))
     API_HASH = os.environ.get("API_HASH", "407aaa4db92ab5d0b32027d6482a5fff")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8750600665:AAEDyDCwIny6q4XGKBqF2Huu_r-kNJ32JrM")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8835661225:AAHH-BJyBGFGVLU549EwvEZglyKxw7jQLIM")
     MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Toxicbots:TOXIC0000@cluster0.u5sllsx.mongodb.net/?appName=Cluster0")
     
     OWNER_ID = int(os.environ.get("OWNER_ID", "8238387029"))
     
     # Void ecosystem logging channel
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003747918681"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003748226916"))
     
     # Premium start image URL
     START_PIC = os.environ.get("START_PIC", "")
